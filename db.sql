9cb25890e0212642f6a2d2d946bb9837c5bf10b0
enstrONGO <75175712+enstrong@users.noreply.github.com>
2025-04-18 16:46:39 +0500

    Add files via upload

--- db.sql
+++ db.sql
@@ -10,7 +10,9 @@ CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
     category_id INTEGER REFERENCES categories(category_id),
     name VARCHAR,
+	description TEXT,
     price NUMERIC,
+	icon VARCHAR,
     stock_quantity INTEGER
 );
 
@@ -53,4 +55,7 @@ INSERT INTO categories (icon, name, description, section) VALUES
 ('Shoes', 'Cycling Shoes', 'Lightweight and efficient performance footwear', 'shoes'),
 ('Bottle', 'Accessories', 'Water bottles, bags, and essential cycling gear', 'accessories');
 
-select * from categories
+INSERT INTO products (category_id, name, description, price, icon, stock_quantity) VALUES 
+(1, 'MET Trenta 3k', 'The MET Trenta 3K Carbon – Pogacar Special Edition offers elite-level aerodynamics and ventilation. Crafted for pro-level performance and protection.', 800, 'helmetPogacar', 50),
+(1, 'S-Works TT 5', 'The S-Works TT 5 is built for speed, inspired by Remco Evenepoel’s time trial dominance. A helmet that slices through air for maximum efficiency.', 550, 'helmetTT', 500),
+(1, 'Giro Aries Spherical', 'Giro Aries Spherical – Team Visma Special Edition. Lightweight, sleek, and engineered for top-tier safety and airflow.', 500, 'helmetVisma', 10);
